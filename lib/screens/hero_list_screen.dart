8ba988ce29d24466884ea92d1f60ea14dda3f6e8
Anthony Piero Avalos Santos <104176063+Anthony2000AA@users.noreply.github.com>
2024-05-27 20:32:22 -0500

    update project

--- lib/screens/hero_list_screen.dart
+++ lib/screens/hero_list_screen.dart
@@ -1,64 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:superhero_mobile_flutter/models/hero.dart';
 import 'package:superhero_mobile_flutter/services/hero_service.dart';
 
-class HeroListScreen extends StatelessWidget {
-  const HeroListScreen({super.key});
+class HeroListScreen extends StatefulWidget {
+  const HeroListScreen({Key? key});
+
+  @override
+  State<HeroListScreen> createState() => _HeroListScreenState();
+}
+
+class _HeroListScreenState extends State<HeroListScreen> {
+  final _searchController = TextEditingController();
+  String _name = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('SuperHero List'),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(150),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Text(
+                "Search for your favorite hero",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    hintText: 'Search hero',
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      _name = value;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: HeroList()
+      body: HeroList(query: _name),
     );
   }
 }
 
 class HeroList extends StatefulWidget {
-  const HeroList({super.key});
+  const HeroList({Key? key, required this.query});
+  final String query;
 
   @override
   State<HeroList> createState() => _HeroListState();
 }
 
 class _HeroListState extends State<HeroList> {
-
-  List<SuperHero> _heroes=[];
+  List<SuperHero> _heroes = [];
   final _heroService = HeroService();
 
+  @override
+  void initState() {
+    initialize();
+    super.initState();
+  }
 
-  initialize() async{
-    final heroes = await _heroService.getHeroesByName('robin');
+  initialize() async {
+    if (widget.query.isEmpty) {
+      return;
+    }
+    final heroes = await _heroService.getHeroesByName(widget.query);
     setState(() {
       _heroes = heroes;
     });
   }
 
   @override
-  void initState() {
-    initialize();
-    super.initState();
-   
-  }
-
-
-  @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: _heroes.length,
-      itemBuilder:(context, index){
-        return Card(
-          child: ListTile(
-            title: Text(_heroes[index].name),
-            subtitle: Text(_heroes[index].fullName),
-            leading: Image.network(_heroes[index].path),
-          ),
-        );
-      }
-
+    if (widget.query.isEmpty) {
+      return Container(); 
+    }
+    return FutureBuilder<List<SuperHero>>(
+      future: _heroService.getHeroesByName(widget.query),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('El super heroe ${widget.query} no existe'));
+        } else {
+          _heroes = snapshot.data ?? [];
+          return ListView.builder(
+            itemCount: _heroes.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  title: Text(_heroes[index].name),
+                  subtitle: Text(_heroes[index].fullName),
+                  leading: Image.network(_heroes[index].path),
+                ),
+              );
+            },
+          );
+        }
+      },
     );
   }
 }
